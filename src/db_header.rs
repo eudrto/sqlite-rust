9393c13430a2847f60e7f60391f4ec3915d09274
Andras Toth <eudrto@gmail.com>
2024-01-26 22:46:59 +0100

    refactor DBHeader to use FromBEBytes

--- src/db_header.rs
+++ src/db_header.rs
@@ -1,41 +1,27 @@
-use std::fmt::Debug;
-use std::fs::File;
-use std::io::Read;
+use std::{fs::File, io::Read};
+
+use crate::from_be_bytes::from_be_bytes;
 
 #[derive(Debug)]
 pub struct DBHeader {
     pub page_size: u16,
+    pub reserved_size: u8,
     pub page_cnt: u32,
+    pub text_encoding: u32,
 }
 
 impl DBHeader {
     pub fn new(file: &mut File) -> Self {
         let mut header = [0; 100];
-        file.read_exact(&mut header).expect("read header");
+        file.read_exact(&mut header).unwrap();
 
         let db_header = Self {
-            page_size: Self::parse_page_size(&header),
-            page_cnt: Self::parse_page_cnt(&header),
+            page_size: from_be_bytes(&mut &header[16..]),
+            reserved_size: from_be_bytes(&mut &header[20..]),
+            page_cnt: from_be_bytes(&mut &header[28..]),
+            text_encoding: from_be_bytes(&mut &header[56..]),
         };
 
         db_header
-    }
-
-    fn parse_page_size(header: &[u8; 100]) -> u16 {
-        const OFFSET: usize = 16;
-        const SIZE: usize = 2;
-
-        let s = &header[OFFSET..OFFSET + SIZE];
-        let arr: [u8; SIZE] = s.try_into().expect("convert slice to array");
-        u16::from_be_bytes(arr)
-    }
-
-    fn parse_page_cnt(header: &[u8; 100]) -> u32 {
-        const OFFSET: usize = 28;
-        const SIZE: usize = 4;
-
-        let s = &header[OFFSET..OFFSET + SIZE];
-        let arr: [u8; SIZE] = s.try_into().expect("convert slice to array");
-        u32::from_be_bytes(arr)
     }
 }
